b24b283fdc22b1d4f0e85dad5c8c0a48e699ab58
ikerio <deikerio@gmail.com>
2026-04-23 18:45:43 -0600

    chore: configure GitHub Pages static export

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,9 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-<<<<<<< Updated upstream
-  basePath: '/espectro-fm-web',
-=======
   basePath: isGithubActions ? `/${repoName}` : '',
   assetPrefix: isGithubActions ? `/${repoName}/` : '',
   trailingSlash: true,
->>>>>>> Stashed changes
   images: {
     unoptimized: true,
     remotePatterns: [
